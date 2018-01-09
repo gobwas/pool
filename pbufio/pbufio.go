e294a5482d9a8523f8e6b2744ea2185083c838af
Sergey Kamardin <gobwas@gmail.com>
2018-01-09 19:38:15 +0100

    pbufio: fix bug on detecting writer size

--- pbufio/pbufio.go
+++ pbufio/pbufio.go
@@ -41,8 +41,8 @@ type WriterPool struct {
 	pool map[int]*sync.Pool
 }
 
-// NewWriterPool creates new WriterPool which reuses min and max sized buffers.
-// Note that min is ceiled to the next power of two.
+// NewWriterPool creates new WriterPool which reuses buffers from at least min
+// and up to max buffer sizes.
 func NewWriterPool(min, max int) *WriterPool {
 	return &WriterPool{
 		pool: pool.MakePoolMap(min, max),
@@ -50,12 +50,11 @@ func NewWriterPool(min, max int) *WriterPool {
 }
 
 // Get returns bufio.Writer whose buffer has at least size bytes.
-// Note that size could be ceiled to the next power of two.
 func (wp *WriterPool) Get(w io.Writer, size int) *bufio.Writer {
 	n := pool.CeilToPowerOfTwo(size)
 
-	pool, ok := wp.pool[n]
-	if !ok {
+	pool := wp.pool[n]
+	if pool == nil {
 		// No such pool that could store such size.
 		return bufio.NewWriterSize(w, size)
 	}
@@ -68,12 +67,10 @@ func (wp *WriterPool) Get(w io.Writer, size int) *bufio.Writer {
 	return bufio.NewWriterSize(w, n)
 }
 
-// Put takes bufio.Writer for future reuse.
-// It does not reuse bufio.Writer which underlying buffer size is not power of
-// two or is out of pool min/max range.
+// Put takes ownership of bufio.Writer for further reuse.
 func (wp *WriterPool) Put(bw *bufio.Writer) {
-	n := bw.Available()
-	if pool, ok := wp.pool[n]; ok {
+	n := writerSize(bw)
+	if pool := wp.pool[n]; pool != nil {
 		// Should reset even if we do Reset() inside Get().
 		// This is done to prevent locking underlying io.Writer from GC.
 		bw.Reset(nil)
@@ -86,8 +83,8 @@ type ReaderPool struct {
 	pool map[int]*sync.Pool
 }
 
-// NewReaderPool creates new ReaderPool which reuses min and max sized buffers.
-// Note that min is ceiled to the next power of two.
+// NewReaderPool creates new ReaderPool which reuses buffers from at least min
+// and up to max buffer sizes.
 func NewReaderPool(min, max int) *ReaderPool {
 	return &ReaderPool{
 		pool: pool.MakePoolMap(min, max),
@@ -95,12 +92,11 @@ func NewReaderPool(min, max int) *ReaderPool {
 }
 
 // Get returns bufio.Reader whose buffer has at least size bytes.
-// Note that size could be ceiled to the next power of two.
 func (rp *ReaderPool) Get(r io.Reader, size int) *bufio.Reader {
 	n := pool.CeilToPowerOfTwo(size)
 
-	pool, ok := rp.pool[n]
-	if !ok {
+	pool := rp.pool[n]
+	if pool == nil {
 		// No such pool that could store such size.
 		return bufio.NewReaderSize(r, size)
 	}
@@ -113,12 +109,10 @@ func (rp *ReaderPool) Get(r io.Reader, size int) *bufio.Reader {
 	return bufio.NewReaderSize(r, n)
 }
 
-// Put takes bufio.Reader and its size for future reuse.
-// It does not reuse bufio.Reader if size is not power of two or is out of pool
-// min/max range.
+// Put takes ownership of bufio.Reader for further reuse.
 func (rp *ReaderPool) Put(br *bufio.Reader) {
 	size := readerSize(br)
-	if pool, ok := rp.pool[size]; ok {
+	if pool := rp.pool[size]; pool != nil {
 		// Should reset even if we do Reset() inside Get().
 		// This is done to prevent locking underlying io.Reader from GC.
 		br.Reset(nil)
@@ -126,18 +120,28 @@ func (rp *ReaderPool) Put(br *bufio.Reader) {
 	}
 }
 
+// writerSize returns buffer size of the given buffered writer.
+func writerSize(bw *bufio.Writer) int {
+	return bw.Available() + bw.Buffered()
+}
+
+// readerSize returns buffer size of the given buffered reader.
+// NOTE: current implementation reset underlying io.Reader.
+//
+// TODO(gobwas): this workaround should be moved under tag when go 1.10
+//               will be released.
+func readerSize(br *bufio.Reader) int {
+	br.Reset(sizeReader)
+	br.ReadByte()
+	n := br.Buffered() + 1
+	br.Reset(nil)
+	return n
+}
+
+var sizeReader optimisticReader
+
 type optimisticReader struct{}
 
 func (optimisticReader) Read(p []byte) (int, error) {
 	return len(p), nil
-}
-
-// readerSize is a workaround to retreive the bufio.Reader's underlying bytes
-// buffer size.
-//
-// TODO(gobwas): this should be moved under tag when go 1.10 will be released.
-func readerSize(br *bufio.Reader) int {
-	br.Reset(optimisticReader{})
-	br.ReadByte()
-	return br.Buffered() + 1
 }
